3ca8dc3bc7e0d81d70a897f3bf2d9b65842b4d3e
SReateguiUtec <sebastian.reategui@utec.edu.pe>
2025-11-30 21:11:35 -0500

    imagenes fixed

--- src/main/java/com/example/medigo/config/SecurityConfig.java
+++ src/main/java/com/example/medigo/config/SecurityConfig.java
@@ -60,8 +60,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5001", "http://localhost:5000")); // Puertos del front end
-        // configuration.setAllowedOrigins(Arrays.asList("*")); // Solo para el deploy                                                                
+        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:5001", "http://localhost:5000")); // Puertos del front end
+        configuration.setAllowedOrigins(Arrays.asList("*")); // Solo para el deploy                                                                
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
